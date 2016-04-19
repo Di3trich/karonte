9586095130b1a6fb4f5e9de4f092b6e600a6a09a
Jonathan Fuentes <qf.jonathan@gmail.com>
2016-04-19 09:41:53 -0500

    link paris and idiorm

--- app/another/init.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$init = new \Karonte\Init();
-
-return $init;
--- app/url.php
+++ app/url.php
@@ -1,24 +1,9 @@
 <?php
 
-include __DIR__ . '/controlador.php';
-
 $router = new Router('/karonte');
 
 $router->route('/', function () {
     echo "hola mundo";
 });
-
-$router->route('/otro', function () {
-    echo "juan carlos marica";
-});
-
-$router->route('/suma/(\d+)/(\d+)', function ($request, $a, $b) {
-    echo $a * 1 + $b * 1;
-    echo $request->query_param('algo');
-});
-
-//$router->controller('/control/(\w+)', new Controller());
-
-$router->app('/app(/.*)', new \Karonte\App(__DIR__ . '/another'));
 
 return $router;
